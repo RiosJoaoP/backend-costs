69ec8d677de622cd03f869948b46e75ff7896d73
João Paulo Rios <joao.paulo.rios@hotmail.com>
2023-03-29 01:19:14 -0300

    categories implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
+    CategoriesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
